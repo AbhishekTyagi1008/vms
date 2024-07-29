8a4f6e1241012e2b5ca669c2469d162c50398985
AbhishekTyagi1008 <abhishektyagi1008@gmail.com>
2024-07-29 16:23:07 +0530

    firstcommit_login done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { LocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { ServicemanagerComponent } from './servicemanager/servicemanager/servicemanager.component';
 
-const routes: Routes = [];
+const routes: Routes = 
+[
+  {path:'',component:LoginComponent},
+  {path:'admin',component:AdminComponent},
+  {path:'servicemanager',component:ServicemanagerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/servicemanager/servicemanager.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ServicemanagerComponent } from './servicemanager/servicemanager.component';
+
+
+
+@NgModule({
+  declarations: [
+    ServicemanagerComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ServicemanagerModule { }
